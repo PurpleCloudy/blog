084f2a75d59afea57332b5d39661e20b26eeb762
Ilya <forstudy546@gmail.com>
2023-05-05 21:43:03 +0300

    class based post list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 
 # Create your views here.
 
@@ -13,18 +15,26 @@ def post_detail(request, year, month, day, post):
                              publish__day = day)
     return render(request, 'blog/post/detail.html', {'post':post})
 
-def post_list(request):
-    post_list = Post.objects.all()
-    #постраничная разбивка по 3 поста на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    #второе число в строке выше - дефолтное значение
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    #paginator.num_pages возвращает число,
-    #которое является количеством страниц
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts':posts})
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     #постраничная разбивка по 3 поста на страницу
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     #второе число в строке выше - дефолтное значение
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     #paginator.num_pages возвращает число,
+#     #которое является количеством страниц
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     return render(request, 'blog/post/list.html', {'posts':posts})
+
+#делает то же, что и закоментированная функция,
+#только в виде класса
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
